488e515c343e44e4f64cd63323444c18f3fda94b
levi-45 <levi45@hotmail.co.uk>
2024-08-16 18:26:46 +0100

    r23

--- installer.sh
+++ installer.sh
@@ -2,8 +2,8 @@
 
 ## setup command=wget -q "--no-check-certificate" https://raw.githubusercontent.com/levi-45/Addon/main/installer.sh -O - | /bin/sh
 ## Only This 2 lines to edit with new version ######
-version='10.1-r22'
-changelog='Fix All known issues'
+version='10.1-r23'
+changelog='Fix Update issue'
 ##
 TMPPATH=/tmp/Addon-main
 FILEPATH=/tmp/main.tar.gz
